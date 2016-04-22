825250944a710029d6ee1955938ad5ce78257898
Mihai Smolenschi <msmolenschi@endava.net>
2016-04-22 09:15:38 +0300

    Contact.html

---
+++ Homework1/JS/endava.js
@@ -0,0 +1,70 @@
+function changePicture(n) {
+	var a = document.getElementById("image");
+	if (n==1) {
+		$('#button1').css('background-color', 'red');
+		$('#button2').css('background-color', 'blue');
+		a.src = "Media/image1.jpg"
+	} else{
+		a.src = "Media/image2.jpg"
+	}
+}
+
+function SubmitMessage(){
+	
+	var validationResponse = ValidateFields();
+	if (validationResponse != "Ok") {
+		alert(validationResponse);
+	} else {
+		alert("Message sent");
+		//do some stuff
+		//ajax call
+	}	
+}
+
+function ValidateFields(){
+	
+	var email = $("#email").val();
+	var name = $("#name").val();
+	var website = $("#website").val();
+	var message = $("#message").val();
+	
+	console.log(email);
+	console.log(name);
+	console.log(website);
+	console.log(message);
+	
+	if (validateEmail(email) == false) {
+		return "Invalid Email Adress";
+	} 
+	if (name == "" || name.length < 5) {
+		return "Invalid Name";
+	}
+	if (isValidWebsite(website) == false) {
+		return "Invalid Website";
+	}
+	if (message == "" || message.length < 10) {
+		return "Invalid Message"
+	}	
+	
+	return "Ok";
+}
+
+function onClickTwitter(){
+	document.location.href = 'https://twitter.com/endava';
+}
+
+function onClickFacebook(){
+	document.location.href = 'https://www.facebook.com/endava/';
+}
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+
+function isValidWebsite(url) {
+	var re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    return re.test(url);
+}
